10e189585bd3ad101453f78bd5de06bcb7286788
bprlab <bprlab@10.200.144.21>
2019-10-31 21:50:43 +0300

    hesaplama yapıldı

---
+++ kadir_yeni/src/main/java/kadir_yeni/Hesaplama.java
@@ -0,0 +1,29 @@
+package kadir_yeni;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+public class Hesaplama {
+
+	public static String topla(String sayi1, String sayi2) {
+		try {
+			BigDecimal bigDecimal1 = new BigDecimal(sayi1);
+			BigDecimal bigDecimal2 = new BigDecimal(sayi2);
+			return bigDecimal1.add(bigDecimal2).toPlainString();
+		} catch (Exception e) {
+			return "undefined";
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner konsolOku = new Scanner(System.in);
+		System.out.print("Sayi 1 giriniz:");
+		String sayi1 = konsolOku.nextLine();
+		System.out.print("Sayi 2 giriniz:");
+		String sayi2 = konsolOku.nextLine();
+
+		String sonuc = Hesaplama.topla(sayi1, sayi2);
+		System.out.printf("%s + %s = %s", sayi1, sayi2, sonuc);
+	}
+
+}
